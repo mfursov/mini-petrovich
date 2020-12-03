620ad5ffc15eef36abe231c41b34b5fe12c47c45
sszuev <sss.zuev@gmail.com>
2020-12-03 20:13:56 +0300

    petrovich4j: change visibility from package-local to public (Library, Petrovich#findRule, Petrovich#applyMode) -- issue petrovich4j#13

--- src/main/java/com/github/petrovich4j/RuleSet.java
+++ src/main/java/com/github/petrovich4j/RuleSet.java
@@ -1,5 +1,7 @@
 package com.github.petrovich4j;
 
+import java.util.Arrays;
+
 public final class RuleSet {
     /**
      * Исключения
@@ -10,9 +12,9 @@ public final class RuleSet {
      */
     public final Rule[] suffixes;
 
-
     public RuleSet(Rule[] exceptions, Rule[] suffixes) {
-        this.exceptions = exceptions;
-        this.suffixes = suffixes;
+        // make a copy to have truly immutable object
+        this.exceptions = Arrays.copyOf(exceptions, exceptions.length);
+        this.suffixes = Arrays.copyOf(suffixes, suffixes.length);
     }
 }
